90a6ba870eefc0342b5430331c3f5eb40666f8de
holliemaethomas <holliemaethomas85@gmail.com>
2018-11-24 16:29:16 -0800

    working

--- data/server.js
+++ data/server.js
@@ -2,7 +2,6 @@
 // const require = ('require');
 const superagent = require('superagent');
 const express = require('express');
-
 const app = express();
 
 const PORT = process.env.PORT || 8080;
@@ -13,37 +12,29 @@ app.set('view-engine', 'ejs');
 app.use(express.static('./public'));
 
 app.get('/', homepage);
-app.get('/categories', categoryPage);
-app.get('/categories/:name', productsPage);
-
-
-
-app.listen(API, () => {
-  console.log('SERVER UP AT ', API);
-});
+// app.get('/categories', categorypage);
+// app.get('/categories/:name', productspage);
 
 // get helper middleware stuff here.
 
-function homePage(request, response) {
-  response.render('site', { page: './views/site', title: 'Our Site: Home' });
+function homepage (request, response) {
+  response.render('site', { page: '../views/pages/index', title: 'Our Site: Home' });
 }
 
-function categoryPage(request, response) {
-  superagent.get(`${API}/categories`)
-    .then(data => {
-      response.render('site', { category: data.body, page: './pages/teams', title: 'categories' });
+// function categorypage(request, response) {
+//   superagent.get(`${API}/categories`)
+//     .then(data => {
+//       response.render('site', { category: data.body, page: './pages/teams', title: 'categories' });
+//     })
+//     .catch(error => console.error(error));
+// }
 
-    });
-    .catch(error => console.error(error));
-  };
-  
-  function productPage(request, response) {
-    superagent.get(`${API}/productPage`)
-      .then(data => {
-        response.render('site', { products: data.body, page: './pages/products', title: 'products' });
+// function productpage (request, response) {
+//   superagent.get(`${API}/productPage`)
+//     .then(data => {
+//       response.render('site', { products: data.body, page: './pages/products', title: 'products' });
+//     })
+//     .catch(error => console.error(error));
 
-      });
-      .catch(error => console.error(error));
-
-      app.listen(PORT, () => console.log('server up on ${PORT}'));
-    };
+//   }
+app.listen(PORT, () => console.log(`server up on ${PORT}`));
